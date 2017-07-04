c7dd4f2df234442a1a7d1d8eabaccddbddbef986
sebastien <sebbeweb@gmail.com>
2017-07-04 17:06:53 +0200

    layout fait

---
+++ app/src/main/java/fr/fondespierre/beweb/mobile/apprenants/DetailApprenantActivity.java
@@ -0,0 +1,17 @@
+package fr.fondespierre.beweb.mobile.apprenants;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sebastien on 04/07/17.
+ */
+
+public class DetailApprenantActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_apprenant);
+    }
+}
